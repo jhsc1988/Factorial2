813d81e8e17dc7309aed1ec1e4fa5d1c1fc58169
Jasmin Husić <48136663+jhsc1988@users.noreply.github.com>
2022-11-02 19:19:33 +0100

    Add project files.

---
+++ UnitTests/FactorialTest.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using System;
+using Vsite.Pood;
+namespace UnitTests
+{
+    [TestClass]
+    public class FactorialTest
+    {
+        [TestMethod]
+        public void MethodReturn1ForArgument1()
+        {
+            Assert.AreEqual(1, Math.Factorial(1));
+        }
+
+        [TestMethod]
+        public void MethodReturn2ForArgument2()
+        {
+            Assert.AreEqual(2, Math.Factorial(2));
+        }
+
+        [TestMethod]
+        public void MethodReturn6ForArgument3()
+        {
+            Assert.AreEqual(6, Math.Factorial(3));
+        }
+
+        [TestMethod]
+        public void MethodReturn120ForArgument5()
+        {
+            Assert.AreEqual(120, Math.Factorial(5));
+        }
+
+        [TestMethod]
+        public void MethodReturn1ForArgument0()
+        {
+            Assert.AreEqual(1, Math.Factorial(0));
+        }
+
+        [TestMethod]
+        public void MethodThrowsExceptionForNegativeArgument()
+        {
+            try
+            {
+                Math.Factorial(-1);
+                Assert.Fail();
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                Assert.IsTrue(true);
+            }
+        }       
+        
+        [TestMethod]
+        public void MethodThrowsExceptionForATooLargeArgument()
+        {
+            try
+            {
+                Math.Factorial(120);
+                Assert.Fail();
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+    }
+}
---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c241e3e0-5c9c-4574-8ad5-fce81a97a06e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
